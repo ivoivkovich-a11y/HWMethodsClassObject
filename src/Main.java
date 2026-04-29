4308e30f54776db8cc205e1104870a7e48eaf327
ivoivkovich <ivoivkovich@gmail.com>
2026-04-29 12:22:22 +0300

    HW V1

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+    Product product1 = new Product(12, "Помидоры", 350, "Овощи");
+    Product product2 = new Product(12, "Помидоры", 350, "Овощи");
+    System.out.println(product1.toString());
+    System.out.println(product2.toString());
+    Product product3 = new Product(13, "Картошка", 45, "Овощи");
+    System.out.println(product3.toString());
+    System.out.println("Objects.equals(product1,product2) = " + Objects.equals(product1, product2));
+    System.out.println("Objects.equals(product1,product3) = " + Objects.equals(product1, product3));
+    System.out.println("Objects.equals(product2,product3) = " + Objects.equals(product2, product3));
+    Order order1 = new Order("Vitya", new Product[]{product1, product3});
+    Order order2 = new Order("Vitya", new Product[]{product1, product3});
+    System.out.println("order1 = " + order1);
+    System.out.println("order2 = " + order2);
+    System.out.println("order1.equals(order2) = " + order1.equals(order2));
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
 }
---
+++ src/Product.java
@@ -0,0 +1,38 @@
+import java.util.Objects;
+
+public class Product {
+    public Product(int id, String name, int price, String category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        counter++;
+
+    }
+    public int id;
+    public String name;
+    public int price;
+    public String category;
+    private static int counter = 0;
+
+
+    
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Product product = (Product) o;
+        return (id == product.id) && Objects.equals(category, product.category);
+
+    }
+
+    public String toString() {
+        return "Товар[артикул=" + this.id +
+               ", название=" + this.name +
+               ", цена=" + this.price +
+               ", категория=" + this.category + "]";
+    }
+}
